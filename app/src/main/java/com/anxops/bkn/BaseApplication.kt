0cb6cf5d8d2e263b159cc0b899e65a1d269c157c
Anxo Pinheiro <anxo@anxops.com>
2023-06-20 10:06:20 +0200

    Remove unused code

--- app/src/main/java/com/anxops/bkn/BaseApplication.kt
+++
@@ -1,5 +0,0 @@
-package com.anxops.bkn
-
-import android.app.Application
-
-open class BaseApplication: Application()
--- app/src/main/java/com/anxops/bkn/MainActivity.kt
+++ app/src/main/java/com/anxops/bkn/MainActivity.kt
@@ -2,21 +2,11 @@ package com.anxops.bkn
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import dagger.hilt.android.AndroidEntryPoint
-import com.anxops.bkn.ui.theme.AndroidcomposetemplateTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AndroidcomposetemplateTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                }
-            }
-        }
     }
 }
