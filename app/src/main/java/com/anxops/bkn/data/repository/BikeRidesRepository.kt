d644b66ef197ca6f887a1f0a8496c41b7871ae44
angelpinheiro <angelpinheirosouto@gmail.com>
2023-07-04 15:29:48 +0200

    Update ride repository

--- app/src/main/java/com/anxops/bkn/data/repository/BikeRidesRepository.kt
+++ app/src/main/java/com/anxops/bkn/data/repository/BikeRidesRepository.kt
@@ -7,8 +7,6 @@ import com.anxops.bkn.data.network.Api
 import com.anxops.bkn.data.network.ApiResponse
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 
 interface RidesRepositoryFacade {
@@ -17,10 +15,6 @@ interface RidesRepositoryFacade {
 
     suspend fun getRides(): List<BikeRide>
 
-    suspend fun getRidesFlow(): Flow<List<BikeRide>>
-
-    suspend fun getBikeRidesFlow(bikeId: String): Flow<List<BikeRide>>
-
     suspend fun getLastBikeRides(bikeId: String): List<BikeRide>
 
     suspend fun updateRide(ride: BikeRide)
@@ -28,11 +22,12 @@ interface RidesRepositoryFacade {
     suspend fun reloadData(): Boolean
 }
 
-class BikeRidesRepository(val api: Api, val db: AppDb, private val defaultDispatcher: CoroutineDispatcher = Dispatchers.IO) : RidesRepositoryFacade {
+class BikeRidesRepository(
+    val api: Api, val db: AppDb, private val defaultDispatcher: CoroutineDispatcher = Dispatchers.IO
+) : RidesRepositoryFacade {
 
-    override suspend fun reloadData(): Boolean {
-
-        val success = when (val rides = api.getRides()) {
+    override suspend fun reloadData(): Boolean = withContext(defaultDispatcher) {
+        when (val rides = api.getRides()) {
             is ApiResponse.Success -> {
                 db.bikeRideDao().clear()
                 rides.data.let {
@@ -45,46 +40,28 @@ class BikeRidesRepository(val api: Api, val db: AppDb, private val defaultDispat
 
             else -> false
         }
-
-        return success
-
     }
 
-    override suspend fun getRidesFlow(): Flow<List<BikeRide>> {
-        return db.bikeRideDao().flow().map {
-            it.map { b ->
-                b.toDomain()
-            }
-        }
-    }
-
-
-
-    override suspend fun getBikeRidesFlow(bikeId: String): Flow<List<BikeRide>> {
-        return db.bikeRideDao().bikeFlow(bikeId).map {
-            it.map { b ->
-                b.toDomain()
-            }
-        }
-    }
-
-    override suspend fun getLastBikeRides(bikeId: String): List<BikeRide> = withContext(defaultDispatcher) {
-        db.bikeRideDao().lastBikeRides(bikeId).map {
+    override suspend fun getLastBikeRides(bikeId: String): List<BikeRide> =
+        withContext(defaultDispatcher) {
+            db.bikeRideDao().lastBikeRides(bikeId).map {
                 it.toDomain()
+            }
         }
-    }
 
     override suspend fun updateRide(ride: BikeRide) {
-        db.bikeRideDao().update(ride.toEntity())
-        api.updateRide(ride)
+        withContext(defaultDispatcher) {
+            db.bikeRideDao().update(ride.toEntity())
+            api.updateRide(ride)
+        }
     }
 
-    override suspend fun getRide(id: String): BikeRide? {
-        return db.bikeRideDao().getById(id)?.toDomain()
+    override suspend fun getRide(id: String): BikeRide? = withContext(defaultDispatcher) {
+        db.bikeRideDao().getById(id)?.toDomain()
     }
 
-    override suspend fun getRides(): List<BikeRide> {
-        return db.bikeRideDao().findAll().map { it.toDomain() }
+    override suspend fun getRides(): List<BikeRide> = withContext(defaultDispatcher) {
+        db.bikeRideDao().findAll().map { it.toDomain() }
     }
 
 }
