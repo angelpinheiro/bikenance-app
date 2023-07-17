240726a13a63dc9ee96823af244fc36320496c51
angelpinheiro <angelpinheirosouto@gmail.com>
2023-07-17 07:45:50 +0200

    Remove unused endpoints

--- app/src/main/java/com/anxops/bkn/data/network/ApiEndpoints.kt
+++ app/src/main/java/com/anxops/bkn/data/network/ApiEndpoints.kt
@@ -7,25 +7,17 @@ object ApiEndpoints {
     private var baseApiUrl = BuildConfig.API_URL + "/api"
 
     fun stravaLoginEndpoint() = "$baseApiUrl/strava"
-
     fun refreshTokenEndpoint() = "$baseApiUrl/refresh"
     fun profileEndpoint() = "$baseApiUrl/profile"
     fun profileBikesEndpoint() = "$baseApiUrl/profile/bikes"
     fun profileRidesEndpoint() = "$baseApiUrl/profile/rides"
     fun profileRidesByKeyEndpoint() = "$baseApiUrl/profile/pagedRides"
     fun profileSyncBikesEndpoint() = "$baseApiUrl/profile/sync"
-
     fun firebaseTokenEndpoint() = "$baseApiUrl/users/messagingToken"
-
-    fun extendedProfileEndpoint(draft: Boolean = false) =
-        "$baseApiUrl/profile/extended?draft=$draft"
-
     fun profileBikeEndpoint(bikeId: String) = "$baseApiUrl/profile/bikes/$bikeId"
-
     fun profileBikeSetupEndpoint(bikeId: String) = "$baseApiUrl/profile/bikes/$bikeId/setup"
     fun profileRideEndpoint(rideId: String) = "$baseApiUrl/profile/rides/$rideId"
     fun addComponentsEndpoint(bikeId: String) = "$baseApiUrl/profile/bikes/$bikeId/components"
-
     fun setUseDebugAPi(use: Boolean) {
         baseApiUrl = if (use) {
             BuildConfig.DEBUG_API_URL + "/api"
