64f452ab8aa92c0d58757df666db83524ca3a8f8
angelpinheiro <angelpinheirosouto@gmail.com>
2023-08-08 08:11:45 +0200

    Update sync bikes screen style

--- app/src/main/java/com/anxops/bkn/ui/screens/bikeSync/BikeSyncScreen.kt
+++ app/src/main/java/com/anxops/bkn/ui/screens/bikeSync/BikeSyncScreen.kt
@@ -1,11 +1,15 @@
 package com.anxops.bkn.ui.screens.bikeSync
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
@@ -35,6 +39,7 @@ fun BikeSyncScreen(
 ) {
     val nav = BknNavigator(navigator)
     val state by viewModel.state.collectAsState()
+    val scrollState = rememberScrollState()
 
 
     LaunchedEffect(state.status) {
@@ -49,65 +54,88 @@ fun BikeSyncScreen(
             BikeSyncScreenStatus.LOADING, BikeSyncScreenStatus.SAVING -> Loading()
             BikeSyncScreenStatus.LOADED -> {
                 Column(
-                    Modifier
-                        .fillMaxSize()
-                        .padding(26.dp),
-                    verticalArrangement = Arrangement.Center
+                    Modifier.fillMaxSize()
                 ) {
 
-                    Text(
-                        text = "STRAVA bike tracking",
-                        style = MaterialTheme.typography.h2,
-                        color = MaterialTheme.colors.onPrimary
-                    )
-
-                    Text(
-                        text = "You have ${state.bikes.size} bikes on Strava. Check the ones you want to track to receive maintenance recommendations.",
-                        style = MaterialTheme.typography.h3,
-                        color = MaterialTheme.colors.onPrimary
-                    )
-
-
-                    Divider(
+                    Column(
                         Modifier
                             .fillMaxWidth()
-                            .height(50.dp)
-                    )
+                            .background(MaterialTheme.colors.primaryVariant)
+                            .padding(26.dp)
+                    ) {
+                        Text(
+                            text = "STRAVA bike tracking",
+                            style = MaterialTheme.typography.h2,
+                            color = MaterialTheme.colors.onPrimary,
+                        )
 
-                    state.bikes.forEach { (bike, sync) ->
+                        Text(
+                            text = "You have ${state.bikes.size} bikes on Strava. Check the ones you want to track to receive maintenance recommendations.",
+                            style = MaterialTheme.typography.h3,
+                            color = MaterialTheme.colors.onPrimary,
 
-                        GarageBikeCard(bike = bike, elevation = 2.dp, topLeftSlot = {
-                            Switch(
-                                checked = sync,
-                                onCheckedChange = { viewModel.syncBike(bike, it) },
-                                colors = SwitchDefaults.colors(
+                        )
+                    }
 
-                                    checkedThumbColor = MaterialTheme.colors.strava,
-                                    uncheckedTrackColor = MaterialTheme.colors.primaryVariant,
-                                    uncheckedThumbColor = MaterialTheme.colors.onPrimary.copy(
-                                        alpha = 0.8f
-                                    )
-                                )
-                            )
-                        }, isLast = true)
+                    Column(
+                        Modifier
+                            .fillMaxSize()
+                            .weight(1f)
+                            .padding(horizontal = 26.dp)
+                            .verticalScroll(scrollState), verticalArrangement = Arrangement.Center
+                    ) {
+
 
                         Divider(
                             Modifier
                                 .fillMaxWidth()
-                                .padding(6.dp)
-                                .height(0.dp)
+                                .height(20.dp)
                         )
+
+                        state.bikes.forEach { (bike, sync) ->
+
+                            GarageBikeCard(bike = bike, elevation = 2.dp, topLeftSlot = {
+                                Switch(
+                                    checked = sync,
+                                    onCheckedChange = { viewModel.syncBike(bike, it) },
+                                    colors = SwitchDefaults.colors(
+
+                                        checkedThumbColor = MaterialTheme.colors.strava,
+                                        uncheckedTrackColor = MaterialTheme.colors.primaryVariant,
+                                        uncheckedThumbColor = MaterialTheme.colors.onPrimary.copy(
+                                            alpha = 0.8f
+                                        )
+                                    )
+                                )
+                            }, isLast = true)
+
+                            Divider(
+                                Modifier
+                                    .fillMaxWidth()
+                                    .padding(6.dp)
+                                    .height(0.dp)
+                            )
+                        }
+
                     }
 
-                    OutlinedButton(
-                        colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.secondary),
-                        onClick = { viewModel.performSync() },
-                        modifier = Modifier
+                    Box(
+                        Modifier
                             .fillMaxWidth()
-                            .padding(horizontal = 16.dp)
-
+                            .background(MaterialTheme.colors.primaryVariant)
+                            .padding(16.dp)
                     ) {
-                        Text(text = "Let's go!", Modifier.padding(5.dp))
+                        OutlinedButton(
+                            onClick = { viewModel.performSync() },
+                            colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.secondary),
+                            modifier = Modifier.fillMaxWidth(),
+                        ) {
+                            Text(
+                                text = "Let's go!",
+                                modifier = Modifier.padding(4.dp),
+                                color = MaterialTheme.colors.onPrimary
+                            )
+                        }
                     }
                 }
             }
